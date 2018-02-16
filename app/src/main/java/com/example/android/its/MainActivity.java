3dcf0c3d0432d25d66e1c34c3568ec9c00c31be1
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-02-16 19:10:20 +0000

    button with link on it

--- app/src/main/java/com/example/android/its/MainActivity.java
+++ app/src/main/java/com/example/android/its/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.its;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goTo (View view) {
+        goToUrl ("https://www.itsligo.ie/");
+    }
+
+    private void goToUrl (String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
     }
 }
